e1c3615fd7f448443b69a1439b2fe2cb53eaad0d
Aleksandr Iksanov <aleksandr.iksanov@al-enterprise.com>
2018-03-12 16:40:20 +0300

    day breaks only if next lesson is absent

--- src/main/java/algorithms/DIPLOMA/util/Population.java
+++ src/main/java/algorithms/DIPLOMA/util/Population.java
@@ -4,66 +4,43 @@ import algorithms.DIPLOMA.model.Genome;
 import algorithms.DIPLOMA.model.Teacher;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Population {
 
     private static final int POPULATION_SIZE = 10;
+    private static List<Teacher> GENES_POOL;
+    private static int[] availableTeachersPerLesson;
     public static final double ELITE_RATE = 0.1;
     public static final double SURVIVE_RATE = 0.5;
     public static final double MUTATION_RATE = 0.2;
-    public static final int TARGET_SIZE = 5;
+    public static int TARGET_SIZE = 5;
 
-    private ArrayList<Teacher> GENES_POOL;
-
-
-    public Population(ArrayList<Teacher> GENES_POOL) {
-        this.GENES_POOL = GENES_POOL;
+    public Population() {
     }
 
-    public List init(int dayOfTheWeek) {
+    public List init() {
+        TARGET_SIZE = calcTargetSize();
         List<Genome> population = new ArrayList<>();
 
         for (int i = 0; i < POPULATION_SIZE; i++) {
             ArrayList<Teacher> teachers = new ArrayList<>();
 
             for (int j = 0; j < TARGET_SIZE; j++) {
-                Teacher teacher = randomTeacher();
-                while (teacher.getDayOfTheWeek() != dayOfTheWeek) {
-                    teacher = randomTeacher();
-                }
-                teachers.add(teacher);
+                teachers.add(randomTeacher());
             }
-
             population.add(new Genome(teachers));
         }
-
         return population;
     }
 
-    private Teacher randomTeacher(){
-        return GENES_POOL.get((int) (Math.random() * GENES_POOL.size() - 1));
-    }
-
-    private Genome arrayModify(Genome g1, Genome g2, int spos) {
-        ArrayList<Teacher> teachers = g1.getDay();
-
-        for (int i = spos; i < teachers.size(); i++) {
-            teachers.set(i, g2.getDay().get(i));
+    private int calcTargetSize(){
+        for (int i = 1; i < availableTeachersPerLesson.length; i++){
+            if(availableTeachersPerLesson[i] == 0){
+                return i - 1;
+            }
         }
-
-        return new Genome(teachers);
-    }
-
-    public List selectElite(List<Genome> population, int esize) {
-        List<Genome> children = new ArrayList<>();
-
-        for (int i = 0; i < esize; i++) {
-            children.add(new Genome(population.get(i)));
-        }
-
-        return children;
+        return availableTeachersPerLesson.length - 1;
     }
 
     public Genome mutate(Genome genome) {
@@ -75,6 +52,10 @@ public class Population {
         genome.calcFitness();
 
         return new Genome(teachers);
+    }
+
+    private Teacher randomTeacher(){
+        return GENES_POOL.get((int) (Math.random() * GENES_POOL.size() - 1));
     }
 
     public List<Genome> mate(List<Genome> population) {
@@ -91,17 +72,50 @@ public class Population {
             if (Math.random() < MUTATION_RATE) {
                 genome = mutate(genome);
             }
-
             children.add(genome);
         }
         return children;
     }
 
-    public void deleteGenesOfFitPopulationFromPool(Genome genome){
-        genome.getDay().forEach(x -> deleteTeacherFromGenesPool(x.getTeacherId()));
+    public List<Genome> selectElite(List<Genome> population, int esize) {
+        List<Genome> children = new ArrayList<>();
+
+        for (int i = 0; i < esize; i++) {
+            children.add(new Genome(population.get(i)));
+        }
+
+        return children;
     }
 
-    private void deleteTeacherFromGenesPool(int teacherId){
+    private Genome arrayModify(Genome g1, Genome g2, int spos) {
+        ArrayList<Teacher> teachers = g1.getDay();
+
+        for (int i = spos; i < teachers.size(); i++) {
+            teachers.set(i, g2.getDay().get(i));
+        }
+
+        return new Genome(teachers);
+    }
+
+    public void initPool(int dayOfTheWeek){
+        this.GENES_POOL = Genes.getInitialPool(dayOfTheWeek);
+        availableTeachersPerLesson = new int[]{0, 0, 0, 0, 0, 0};
+        getAvailableTeachersPerLesson();
+    }
+
+    private int[] getAvailableTeachersPerLesson(){
+        for (Teacher teacher : GENES_POOL){
+            availableTeachersPerLesson[teacher.getLesson()]++;
+        }
+        return availableTeachersPerLesson;
+    }
+
+    public void deleteGenesFromPool(Genome genome){
+        genome.getDay().forEach(x -> deleteTeacherFromGenesPool(x.getTeacherId(), x.getLesson()));
+    }
+
+    private void deleteTeacherFromGenesPool(int teacherId, int lesson){
         GENES_POOL.removeIf(x -> x.getTeacherId() == teacherId);
+        availableTeachersPerLesson[lesson]--;
     }
 }
