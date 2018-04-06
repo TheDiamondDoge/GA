be344b22fd0afebdc95d20ecb304cfa658cdfd87
Aleksandr Iksanov <aleksandr.iksanov@al-enterprise.com>
2018-04-06 15:32:59 +0300

    hometask

--- src/main/java/algorithms/DIPLOMA/model/Teacher.java
+++ src/main/java/algorithms/DIPLOMA/model/Teacher.java
@@ -2,10 +2,16 @@ package algorithms.DIPLOMA.model;
 
 
 public class Teacher {
+    //TODO Rewrite unit tests using MOCK
+    //TODO Map<Grade, Map<Subject, Amount>. New class that parses xlsx with learning plan to this map.
+    //TODO Think about how to implement that
+    //TODO Make writer for another sheet
+    //TODO If all work - you are succeeded
     private String name;
     private int lesson;
     private int dayOfTheWeek;
     private String grade;
+    private String subjectName;
 
 
     public Teacher(String name, int lesson, int dayOfTheWeek, String grade) {
@@ -29,6 +35,14 @@ public class Teacher {
 
     public String getGrade() {
         return grade;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
     }
 
     @Override
